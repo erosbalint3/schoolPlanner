0e31fde0f98cdb32ee02b92eeeddee48dc41cfc6
Eros Balint <erosbalint3@gmail.com>
2023-08-31 18:47:38 +0200

    AddGradeForm

---
+++ school-planner/src/Grades/AddGradeForm/AddGradeForm.tsx
@@ -0,0 +1,20 @@
+import React from 'react';
+
+const AddGradeForm = () => {
+    return (
+        <div id='mainAddGradeFormDiv'>
+            <div className='FormDiv'>
+                <div></div>
+                <div></div>
+                <div></div>
+                <div></div>
+                <div></div>
+            </div>
+            <div className='AddButton'>
+
+            </div>
+        </div>
+    );
+}
+
+export default AddGradeForm;
--- school-planner/src/Grades/Grades.tsx
+++ school-planner/src/Grades/Grades.tsx
@@ -5,9 +5,12 @@ import SubjectMenu from "./SubjectMenu/SubjectMenu";
 const Grades = () => {
     return (
         <div id="mainGradesDiv">
-            <div id="gradesDiv">
-                <SubjectMenu />
+            <div id="outer">
+                <div id="gradesDiv">
+                    <SubjectMenu />
+                </div>
             </div>
+            
             <div id="addGradeDiv">
 
             </div>
